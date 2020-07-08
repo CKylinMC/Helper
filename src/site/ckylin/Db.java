01dcd79b5968836af8e4c9929b88a7d94bf02f0a
CKylinMC <canszj@yeah.net>
2020-07-08 13:22:33 +0800

    添加注释

--- src/site/ckylin/Db.java
+++ src/site/ckylin/Db.java
@@ -7,38 +7,80 @@
 package site.ckylin;
 
 import javax.sql.rowset.CachedRowSet;
-import javax.sql.rowset.RowSetProvider;
 import java.sql.*;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 
+/**
+ * 数据库代理类 for MySQL
+ */
 public class Db {
     private Connection conn;
     private String lastSql;
     private int execCount = 0;
 
+    /**
+     * 使用已有连接初始化代理类
+     *
+     * @param conn the conn
+     */
     public Db(Connection conn) {
         this.conn = conn;
     }
 
+    /**
+     * 使用本地默认地址和端口号初始化新的数据库连接和代理类
+     *
+     * @param username the username
+     * @param password the password
+     * @param database the database
+     */
     public Db(String username, String password, String database) {
         this.conn = DbHelper.initConnection(username, password, database);
     }
 
+    /**
+     * 使用MySQL默认端口号(3306)初始化新的数据库连接和代理类
+     *
+     * @param username the username
+     * @param password the password
+     * @param database the database
+     * @param hostname the hostname
+     */
     public Db(String username, String password, String database, String hostname) {
         this.conn = DbHelper.initConnection(username, password, database, hostname);
     }
 
+    /**
+     * 初始化新的数据库连接和代理类
+     *
+     * @param username the username
+     * @param password the password
+     * @param database the database
+     * @param hostname the hostname
+     * @param port     the port
+     */
     public Db(String username, String password, String database, String hostname, int port) {
         this.conn = DbHelper.initConnection(username, password, database, hostname, port);
     }
 
+    /**
+     * 获取数据库连接对象
+     *
+     * @return the connection
+     */
     public Connection getConnection() {
         return conn;
     }
 
+    /**
+     * 执行“修改”查询，返回受影响的行数
+     *
+     * @param SQL 查询语句
+     * @return 受影响的行数，-1为出错
+     * @throws SQLException 数据库错误
+     */
     public int execUpdate(String SQL) throws SQLException {
         if (!isOk()) return -1;
         lastSql = SQL;
@@ -50,6 +92,13 @@ public class Db {
         return res;
     }
 
+    /**
+     * 执行“查询”，返回结果
+     *
+     * @param SQL 查询语句
+     * @return 缓存的结果集
+     * @throws SQLException 数据库错误
+     */
     public CachedRowSet execQuery(String SQL) throws SQLException {
         if (!isOk()) return null;
         lastSql = SQL;
@@ -63,6 +112,13 @@ public class Db {
         return cachedRowSet;
     }
 
+    /**
+     * 执行语句，返回执行成功与否
+     *
+     * @param SQL 查询语句
+     * @return 是否成功执行
+     * @throws SQLException 数据库错误
+     */
     public boolean exec(String SQL) throws SQLException {
         if (!isOk()) return false;
         lastSql = SQL;
@@ -74,25 +130,48 @@ public class Db {
         return res;
     }
 
+    /**
+     * 获取一个新的预查询对象
+     *
+     * @param sql 要执行的查询语句，可包含占位符
+     * @return 预查询对象
+     * @throws SQLException 数据库错误
+     */
     public PreparedStatement getPreStatement(String sql) throws SQLException {
         return conn.prepareStatement(sql);
     }
 
-    public int execUpdate(String SQL, Object[] params) throws SQLException {
+    /**
+     * 使用“预查询”方式执行“修改”查询，返回受影响的行数
+     *
+     * @param SQL    可以包含占位符的查询语句
+     * @param params 占位符替换参数
+     * @return 受影响的行数
+     * @throws SQLException 数据库错误
+     */
+    public int execUpdate(String SQL, Object... params) throws SQLException {
         if (!isOk()) return -1;
         lastSql = SQL;
         execCount++;
         int res;
         try (PreparedStatement stmt = getPreStatement(SQL)) {
             for (int i = 1; i <= params.length; i++) {
-                stmt.setObject(i, params[i]);
+                stmt.setObject(i, params[i - 1]);
             }
-            res = stmt.executeUpdate(SQL);
+            res = stmt.executeUpdate();
         }
         return res;
     }
 
-    public CachedRowSet execQuery(String SQL, Object[] params) throws SQLException {
+    /**
+     * 使用“预查询”方式执行查询，返回结果
+     *
+     * @param SQL    可以包含占位符的查询语句
+     * @param params 占位符替换参数
+     * @return 缓存的结果集
+     * @throws SQLException 数据库错误
+     */
+    public CachedRowSet execQuery(String SQL, Object... params) throws SQLException {
         if (!isOk()) return null;
         lastSql = SQL;
         execCount++;
@@ -100,45 +179,89 @@ public class Db {
         CachedRowSet cachedRowSet;
         try (PreparedStatement stmt = getPreStatement(SQL)) {
             for (int i = 1; i <= params.length; i++) {
-                stmt.setObject(i, params[i]);
+                stmt.setObject(i, params[i - 1]);
             }
-            res = stmt.executeQuery(SQL);
+            res = stmt.executeQuery();
             cachedRowSet = DbHelper.toRowSet(res);
         }
         return cachedRowSet;
     }
 
-    public boolean exec(String SQL, Object[] params) throws SQLException {
+    /**
+     * 使用“预查询”方式执行查询，返回执行是否成功的状态
+     *
+     * @param SQL    可以包含占位符的查询语句
+     * @param params 占位符替换参数
+     * @return 是否成功执行
+     * @throws SQLException 数据库错误
+     */
+    public boolean exec(String SQL, Object... params) throws SQLException {
         if (!isOk()) return false;
         lastSql = SQL;
         execCount++;
         boolean res;
         try (PreparedStatement stmt = getPreStatement(SQL)) {
             for (int i = 1; i <= params.length; i++) {
-                stmt.setObject(i, params[i]);
+                stmt.setObject(i, params[i - 1]);
             }
-            res = stmt.execute(SQL);
+            res = stmt.execute();
         }
         return res;
     }
 
+    /**
+     * 查询所有列
+     *
+     * @param tableName 数据表名
+     * @return 缓存的结果集
+     */
     public CachedRowSet SELECT(String tableName) {
         return SELECT("*", tableName, null);
     }
 
+    /**
+     * 查询所有数据
+     *
+     * @param tableName 数据表名
+     * @param options   参数
+     * @return 缓存的结果集
+     */
     public CachedRowSet SELECT(String tableName, String options) {
         return SELECT("*", tableName, options);
     }
 
+    /**
+     * 查询数据
+     *
+     * @param columns   指定数据列
+     * @param tableName 数据表名
+     * @return 缓存的结果集
+     */
     public CachedRowSet SELECT(ArrayList<String> columns, String tableName) {
         return SELECT(columns, tableName, null);
     }
 
+    /**
+     * 查询数据
+     *
+     * @param columns   指定数据列
+     * @param tableName 数据表名
+     * @param options   参数
+     * @return 缓存的结果集
+     */
     public CachedRowSet SELECT(ArrayList<String> columns, String tableName, String options) {
         String cols = Helper.arrayJoin(columns, ",");
         return SELECT(cols, tableName, options);
     }
 
+    /**
+     * 查询数据
+     *
+     * @param columns   指定数据列
+     * @param tableName 数据表名
+     * @param options   参数
+     * @return 缓存的结果集
+     */
     public CachedRowSet SELECT(String columns, String tableName, String options) {
         try {
             return execQuery(String.format("SELECT %s from %s %s", columns, tableName, options == null ? "" : options));
@@ -148,6 +271,13 @@ public class Db {
         return null;
     }
 
+    /**
+     * 插入数据
+     *
+     * @param tableName 数据表名
+     * @param datas     数据
+     * @return 受影响行数
+     */
     public int INSERT(String tableName, ArrayList<String> datas) {
         StringBuilder data = new StringBuilder();
         for (int i = 0; i < datas.size(); i++) {
@@ -161,10 +291,26 @@ public class Db {
         return -1;
     }
 
+    /**
+     * 插入数据
+     *
+     * @param tableName 数据表名
+     * @param columns   指定数据列
+     * @param datas     数据
+     * @return 受影响行数
+     */
     public int INSERT(String tableName, ArrayList<String> columns, ArrayList<String> datas) {
         return INSERT(tableName, Helper.arrayJoin(columns, ","), datas);
     }
 
+    /**
+     * 插入数据
+     *
+     * @param tableName 数据表名
+     * @param columns   指定数据列
+     * @param datas     数据
+     * @return 受影响行数
+     */
     public int INSERT(String tableName, String columns, ArrayList<String> datas) {
         StringBuilder data = new StringBuilder();
         for (int i = 0; i < datas.size(); i++) {
@@ -178,10 +324,25 @@ public class Db {
         return -1;
     }
 
+    /**
+     * 更新数据，自动添加引号
+     *
+     * @param tableName 数据表名
+     * @param set       修改数据
+     * @return 受影响行数
+     */
     public int UPDATE_quoted(String tableName, HashMap<String, String> set) {
         return UPDATE_quoted(tableName, set, "");
     }
 
+    /**
+     * 更新数据，自动添加引号
+     *
+     * @param tableName 数据表名
+     * @param set       修改数据
+     * @param options   参数
+     * @return 受影响行数
+     */
     public int UPDATE_quoted(String tableName, HashMap<String, String> set, String options) {
         StringBuilder setstr = new StringBuilder();
         for (Map.Entry<String, String> setItem : set.entrySet()) {
@@ -191,10 +352,25 @@ public class Db {
         return UPDATE(tableName, setstr.toString(), options);
     }
 
+    /**
+     * 更新数据，不添加引号
+     *
+     * @param tableName 数据表名
+     * @param set       修改数据
+     * @return 受影响行数
+     */
     public int UPDATE(String tableName, HashMap<String, String> set) {
         return UPDATE(tableName, set, "");
     }
 
+    /**
+     * 更新数据，不添加引号
+     *
+     * @param tableName 数据表名
+     * @param set       修改数据
+     * @param options   参数
+     * @return 受影响行数
+     */
     public int UPDATE(String tableName, HashMap<String, String> set, String options) {
         StringBuilder setstr = new StringBuilder();
         for (Map.Entry<String, String> setItem : set.entrySet()) {
@@ -204,6 +380,14 @@ public class Db {
         return UPDATE(tableName, setstr.toString(), options);
     }
 
+    /**
+     * 更新数据
+     *
+     * @param tableName 数据表名
+     * @param set       修改数据
+     * @param options   参数
+     * @return 受影响行数
+     */
     public int UPDATE(String tableName, String set, String options) {
         try {
             return execUpdate(String.format("UPDATE SET %s %s %s", tableName, set, options == null ? "" : "WHERE " + options));
@@ -213,10 +397,23 @@ public class Db {
         return -1;
     }
 
+    /**
+     * 删除所有数据
+     *
+     * @param tableName 数据表名
+     * @return 受影响行数
+     */
     public int DELETE(String tableName) {
         return DELETE(tableName, "");
     }
 
+    /**
+     * 删除数据
+     *
+     * @param tableName 数据表名
+     * @param where     限定条件
+     * @return 受影响行数
+     */
     public int DELETE(String tableName, String where) {
         try {
             return execUpdate(String.format("DELETE FROM %s %s", tableName, where != null && !where.equals("") ? "WHERE " + where : ""));
@@ -226,6 +423,9 @@ public class Db {
         return -1;
     }
 
+    /**
+     * 关闭数据库连接
+     */
     public void close() {
         try {
             if (isOk()) {
@@ -236,14 +436,30 @@ public class Db {
         }
     }
 
+    /**
+     * 返回连接是否有效
+     *
+     * @return 是否有效
+     * @throws SQLException 数据库错误
+     */
     public boolean isOk() throws SQLException {
         return conn != null && !conn.isClosed();
     }
 
+    /**
+     * Gets last sql.
+     *
+     * @return the last sql
+     */
     public String getLastSql() {
         return lastSql;
     }
 
+    /**
+     * Gets exec count.
+     *
+     * @return the exec count
+     */
     public int getExecCount() {
         return execCount;
     }
--- src/site/ckylin/DbHelper.java
+++ src/site/ckylin/DbHelper.java
@@ -12,9 +12,17 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.ArrayList;
+import java.util.Properties;
 
+/**
+ * 数据库工具类 for MySQL
+ */
 public class DbHelper {
+    /**
+     * 加载 MySQL 驱动
+     *
+     * @return the boolean
+     */
     public static boolean loadDriver() {
         try {
             Class.forName("com.mysql.jdbc.Driver");
@@ -25,19 +33,56 @@ public class DbHelper {
         return false;
     }
 
+    /**
+     * 构建 JDBC URL, 并使用 UTF-8 编码
+     *
+     * @param driver       驱动名(如: mysql)
+     * @param host         主机地址
+     * @param port         端口(MySQL数据库默认端口号为: 3306)
+     * @param databaseName 欲连接的数据库名
+     * @return 已构建的 JDBC URL
+     */
     public static String buildJDBCUrl(String driver, String host, int port, String databaseName) {
         return "jdbc:" + driver + "://" + host + ":" + port + "/" + databaseName + "?useUnicode=true&characterEncoding=UTF-8";
     }
 
+    /**
+     * 初始化一个数据库连接，使用本地默认地址和端口号(127.0.0.1:3306)。
+     *
+     * @param username     用户名
+     * @param password     密码
+     * @param databaseName 欲连接的数据库名
+     * @return 数据库连接
+     */
     public static Connection initConnection(String username, String password, String databaseName) {
         return initConnection(username, password, databaseName, "127.0.0.1", 3306);
     }
 
+    /**
+     * 初始化一个数据库连接，使用默认端口号(3306)
+     *
+     * @param username     用户名
+     * @param password     密码
+     * @param databaseName 欲连接的数据库名
+     * @param hostname     主机地址
+     * @return 数据库连接
+     */
     public static Connection initConnection(String username, String password, String databaseName, String hostname) {
         return initConnection(username, password, databaseName, hostname, 3306);
     }
 
+    /**
+     * 初始化一个数据库连接
+     *
+     * @param username     用户名
+     * @param password     密码
+     * @param databaseName 欲连接的数据库名
+     * @param hostname     主机地址
+     * @param port         端口号(MySQL默认端口号为: 3306)
+     * @return 数据库连接
+     */
     public static Connection initConnection(String username, String password, String databaseName, String hostname, int port) {
+        loadDriver();
         try {
             return DriverManager.getConnection(buildJDBCUrl("mysql", hostname, port, databaseName), username, password);
         } catch (SQLException e) {
@@ -46,6 +91,13 @@ public class DbHelper {
         return null;
     }
 
+    /**
+     * 将关联到数据库的 <code>ResultSet</code> 结果集转化为无关联的 <code>CachedRowSet</code> 缓存集。
+     * 提示: 由于 <code>CachedRowSet</code> 实现了 <code>ResultSet</code>, 所以类型定义无需改变。
+     *
+     * @param rs 欲转化的结果集
+     * @return 转化的缓存集
+     */
     public static CachedRowSet toRowSet(ResultSet rs) {
         try {
             CachedRowSet cachedRowSet = RowSetProvider.newFactory().createCachedRowSet();
@@ -57,10 +109,23 @@ public class DbHelper {
         return null;
     }
 
+    /**
+     * 生成查询 “选择” <code>SELECT * FROM [table]</code>
+     *
+     * @param tableName 数据表名
+     * @return 构造好的语句
+     */
     public static String getSelectCmd(String tableName) {
         return getSelectCmd(tableName, "*");
     }
 
+    /**
+     * 生成查询 “选择” <code>SELECT [columns] FROM [table]</code>
+     *
+     * @param tableName 数据表名
+     * @param columns   所有数据列
+     * @return 构造好的语句
+     */
     public static String getSelectCmd(String tableName, String... columns) {
         String baseCmd = "SELECT <COLS> FROM <TBNAME> ";
         String cmd = baseCmd.replace("<TBNAME>", tableName);
@@ -69,13 +134,17 @@ public class DbHelper {
             String delimiter = i == columns.length - 1 ? "" : ",";
             cols.append(columns[i]).append(delimiter);
         }
-        if (cols.length() > 0)
-            cmd = cmd.replace("<COLS>", "(" + cols + ")");
-        else
-            cmd = cmd.replace("<COLS>", "");
+        cmd = cmd.replace("<COLS>", cols);
         return cmd;
     }
 
+    /**
+     * 生成占位符查询 “插入” <code>INSERT INTO [table] ([columns]) VALUES (?,?...)</code>
+     *
+     * @param tableName 数据表名
+     * @param columns   欲插入的列
+     * @return 包含占位符的语句
+     */
     public static String getInsertCmd(String tableName, String... columns) {
         String baseCmd = "INSERT INTO <TBNAME> <COLS> VALUES (<VALS>) ";
         String cmd = baseCmd.replace("<TBNAME>", tableName);
@@ -97,6 +166,13 @@ public class DbHelper {
         return cmd;
     }
 
+    /**
+     * 生成占位符查询 “更新” <code>UPDATE [table] SET [changedColumns=?,...] </code>
+     *
+     * @param tableName      数据表名
+     * @param changedColumns 欲修改的列
+     * @return 包含占位符的语句
+     */
     public static String getUpdateCmd(String tableName, String... changedColumns) {
         String baseCmd = "UPDATE <TBNAME> SET <COLS> ";
         String cmd = baseCmd.replace("<TBNAME>", tableName);
@@ -105,21 +181,71 @@ public class DbHelper {
             String delimiter = i == changedColumns.length - 1 ? "=?" : "=?,";
             cols.append(changedColumns[i]).append(delimiter);
         }
-        if (cols.length() > 0)
-            cmd = cmd.replace("<COLS>", "(" + cols + ")");
-        else
-            cmd = cmd.replace("<COLS>", "");
+        cmd = cmd.replace("<COLS>", cols);
         return cmd;
     }
 
+    /**
+     * 生成查询 “删除” <code>DELETE FROM [table] </code>
+     *
+     * @param tableName 数据表名
+     * @return 查询语句
+     */
     public static String getDeleteCmd(String tableName) {
         String baseCmd = "DELETE FROM <TBNAME> ";
         String cmd = baseCmd.replace("<TBNAME>", tableName);
         return cmd;
     }
 
+    /**
+     * 生成 "WHERE" 限定语句。此方法用于拼接。
+     *
+     * @param limits 所有限定
+     * @return 查询语句
+     */
     public static String where(String limits) {
-        return "WHERE " + limits + " ";
+        return " WHERE " + limits + " ";
+    }
+
+    /**
+     * 生成 "LIMIT" 限定语句。此方法用于拼接。
+     *
+     * @param count 限制行数
+     * @return 查询语句
+     */
+    public static String limit(int count) {
+        return " LIMIT " + count;
+    }
+
+    /**
+     * 生成 "LIMIT" 限定语句。此方法用于拼接。
+     *
+     * @param from 从第几个结果开始
+     * @param to   一共查询几个结果
+     * @return 查询语句
+     */
+    public static String limit(int from, int to) {
+        return " LIMIT " + from + "," + to;
+    }
+
+    /**
+     * 从配置文件读取信息，并初始化数据连接，然后返回数据库代理类。
+     * 配置文件需要包含以下字段：
+     * <ul>
+     *     <li><code>user</code>   => 用户名</li>
+     *     <li><code>pass</code>   => 密码</li>
+     *     <li><code>dbname</code> => 数据库名</li>
+     *     <li><code>host</code>   => 主机地址</li>
+     *     <li><code>port</code>   => 端口号</li>
+     * </ul>
+     *
+     * @param path 配置文件路径
+     * @return 数据库代理类
+     */
+    public static Db connectFromProperties(String path) {
+        Properties properties = Helper.loadProperties(path);
+        if (properties == null) return null;
+        return new Db(properties.getProperty("user"), properties.getProperty("pass"), properties.getProperty("dbname"), properties.getProperty("host"), Helper.str2int(properties.getProperty("port"), 3306));
     }
 }
 
